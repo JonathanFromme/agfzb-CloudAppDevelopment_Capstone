114ad725c82f333fcc77d9cb9be20d0621700fca
JonathanFromme <noj157157@gmail.com>
2023-07-30 14:26:36 -0400

    week3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
 
     # path for registration
     path('registration/', views.registration_request, name='registration'),
+
     # path for login
     path('login/', views.login_request, name='login'),
 
@@ -24,7 +25,9 @@ urlpatterns = [
     path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
